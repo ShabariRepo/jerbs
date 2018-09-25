85de2bbbe713d363e695a2ffd3e1da238454fe36
shabari shenoy <sss_shenoy06@hotmail.com>
2018-09-25 14:29:01 -0400

    pass in REGION prop to position the map on the screen when load - lat & long - lat delta & long delta - second one is for a zoom

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,11 +3,24 @@ import { View, Text } from "react-native";
 import { MapView } from 'expo';
 
 class MapScreen extends Component {
+    // component level state
+    state = {
+        region: {
+            longitude: -79.6010328,
+            latitude: 43.6565353,
+            longitudeDelta: 0.04,
+            latitudeDelta: 1
+        }
+    }
+    
     render(){
         return (
             // expand and fill all the area on the screen with flex 1
             <View style={{ flex: 1 }}>
-                <MapView style={{ flex: 1 }} />
+                <MapView 
+                region={this.state.region}
+                style={{ flex: 1 }} 
+                />
             </View>
         )
     }
