ccc579338e8910f838a6787c525d03157a44669c
shabari shenoy <sss_shenoy06@hotmail.com>
2018-09-25 13:46:45 -0400

    wire up the piece of state to authScreen - redirect them to map screen - if logged in then auto move them to map screen

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -10,22 +10,40 @@ class AuthScreen extends React.Component {
         this.props.facebookLogin();
         // to remove the token when authscreen starts
         //AsyncStorage.removeItem('fb_token');
+
+        // we dont know if this is going to work after fb login is complete
+        // but not neccessary but if you modify the auth flow in anyway and want to come to this screen after authenticating then sure
+        this.onAuthComplete(this.props);
+    }
+
+    // anytime the state changes then all the components that are subscribed to redux will have to re-render with new set of props
+    // lifecycle method that is called when component is JUST about to be re-rendered
+    // captures a case where they successfully log in
+    componentWillReceiveProps(nextProps){
+        this.onAuthComplete(nextProps);
+    }
+
+    // when user completes authorization
+    onAuthComplete(props){
+        if(props.token){
+            // navigation is provided by RN navigation
+            this.props.navigation.navigate('map');
+        }
     }
 
     render(){
         return (
-            <View>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-            </View>
+            <View />
         );
     }
 }
 
+// map state to props : so the auth screen knows what the current authentication state of the user is
+// below is state.auth but destructure it to just ask for auth
+function mapStateToProps({ auth }){
+    return { token: auth.token };
+}
+
 // first argument is always the map state to props -- we dont need any state in auth screen just yet
 // second is the action creator, so in this case take all and bind them to AuthScreen
-export default connect(null, actions) (AuthScreen);
+export default connect(mapStateToProps, actions) (AuthScreen);
