592a85367458b44b7c839867d9d08d3aef22d707
shabari shenoy <sss_shenoy06@hotmail.com>
2018-09-24 23:26:19 -0400

    use redux thunk for the asynchronous calls from action creators - action creators have to NOT be async so to use it u need thunk

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -6,9 +6,19 @@ import {
 // how to use AsyncStorage
 // AsyncStorage.setItem('fb_token', token);
 // AsyncStorage.getItem('fb_token);
-export const facebookLogin = async () => {
+export const facebookLogin = () => {
     // redux is dumped when user closes app
     // so to persist data save to async storage
     // async storage is like LOCAL STORAGE from browser
 
+    return async function (dispatch) {
+        // redux thunk says you can return a function from action creator and thunk will call the function and pass dispatch
+        let token = await AsyncStorage.getItem('fb_token');
+        if (token) {
+            // if token exists dispatch an action saying FB login is done
+        } else {
+            // else start FB login process
+
+        }
+    }
 }
